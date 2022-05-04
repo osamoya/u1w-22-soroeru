148bdce49d7e9d7cb4e3c9b910116dbbc5aab610
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2022-05-04 19:48:31 +0900

    [Add]2種類の弾を撃ち分けできるように

--- Assets/Scripts/GameTestManager_Script.cs
+++ Assets/Scripts/GameTestManager_Script.cs
@@ -6,28 +6,34 @@ using UnityEngine.UI;
 public class GameTestManager_Script: MonoBehaviour
 {
     [SerializeField] ShotSimple_Script ShotSimple_;
-    [SerializeField] Button b_onoff;
-    ShotOnOff_Script shotOnOff_;
+    [SerializeField] Button b_onoff1;
+    [SerializeField] Button b_onoff2;
+    ShotOnOff_Script shotOnOff_1;
+    ShotOnOff_Script shotOnOff_2;
     [SerializeField] int RPS;//1ïbÇ≈åÇÇƒÇÈêî
     long count;
     // Start is called before the first frame update
     void Start()
     {
-        shotOnOff_ = b_onoff.GetComponent<ShotOnOff_Script>();
+        shotOnOff_1 = b_onoff1.GetComponent<ShotOnOff_Script>();
+        shotOnOff_2 = b_onoff2.GetComponent<ShotOnOff_Script>();
         count = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (shotOnOff_.Shooting)
+        if (shotOnOff_1.Shooting)
         {
             if (count % (300/RPS) == 0)
             {
-                ShotSimple_.shot();
+                ShotSimple_.shot(1);
             }
         }
-        else { count = 0; }
+        if (shotOnOff_2.Shooting)
+        {
+            ShotSimple_.shot(2);
+        }
         count++;
     }
 
--- Assets/Scripts/ShotSimple_Script.cs
+++ Assets/Scripts/ShotSimple_Script.cs
@@ -5,21 +5,42 @@ using UnityEngine;
 public class ShotSimple_Script : MonoBehaviour
 {
     public GameObject bullet;
+    public GameObject bullet2;
 
-    public void shot()
+    public void shot()//égÇÌÇ»Ç¢
     {
-        shot(ShotManager_Script.targetPos);
+        shot(1,ShotManager_Script.targetPos);
     }
-    public void shot(float x,float y)
+    public void shot(int n,float x,float y)
     {
-        GameObject b = Instantiate(bullet);
+        GameObject b;
+        switch (n)
+        {
+            case 1: b = Instantiate(bullet); break;
+            case 2: b = Instantiate(bullet2); break;
+            default:b = Instantiate(bullet); break;
+        }
         b.transform.position = this.transform.position;
         b.GetComponent<Rigidbody2D>().AddForce(GetDiff(b.transform.position,new Vector2(x,y)));
     }
-    public void shot(Vector2 v)
+    public void shot(int n, Vector2 v)
     {
-        shot(v.x,v.y);
+        shot(n,v.x,v.y);
     }
+    public  void shot(int num)//î‘çÜéwíË
+    {
+        switch (num)
+        {
+            case 1:
+            case 2:
+                shot(num,ShotManager_Script.targetPos);
+                break;
+            default:
+                Debug.Log("No item");
+                break;
+        }
+    }
+    
 
     Vector2 GetDiff(Vector2 start,Vector2 goal)
     {
@@ -30,8 +51,8 @@ public class ShotSimple_Script : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            shot(ShotManager_Script.targetPos);
-            Debug.Log(ShotManager_Script.targetPos);
+            //shot(ShotManager_Script.targetPos);
+            //Debug.Log(ShotManager_Script.targetPos);
         }
     }
 
