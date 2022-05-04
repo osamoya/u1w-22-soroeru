dc7a4cd32c392823c0ee214453a7bbb46ce96498
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2022-05-05 01:58:03 +0900

    [Add]生成されてから1秒後に削除

---
+++ Assets/Scripts/DestroyBulletSelf_Script.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBulletSelf_Script : MonoBehaviour
+{
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 1.0f) Destroy(this.gameObject);
+    }
+}
