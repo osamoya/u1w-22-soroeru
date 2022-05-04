d62cdccff29102a60f06f05784ed878c34870b37
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2022-05-04 19:30:14 +0900

    [update]発射間隔を変更できるように

--- Assets/Scripts/GameTestManager_Script.cs
+++ Assets/Scripts/GameTestManager_Script.cs
@@ -8,16 +8,28 @@ public class GameTestManager_Script: MonoBehaviour
     [SerializeField] ShotSimple_Script ShotSimple_;
     [SerializeField] Button b_onoff;
     ShotOnOff_Script shotOnOff_;
+    [SerializeField] int RPS;//1ïbÇ≈åÇÇƒÇÈêî
+    long count;
     // Start is called before the first frame update
     void Start()
     {
         shotOnOff_ = b_onoff.GetComponent<ShotOnOff_Script>();
+        count = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (shotOnOff_.Shooting) ShotSimple_.shot();
-        
+        if (shotOnOff_.Shooting)
+        {
+            if (count % (300/RPS) == 0)
+            {
+                ShotSimple_.shot();
+            }
+        }
+        else { count = 0; }
+        count++;
     }
+
+
 }
