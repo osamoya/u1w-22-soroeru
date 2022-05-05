74befe090b67df566dbfcbc1d1591d8e5745d00a
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2022-05-06 00:26:05 +0900

    [update]buy→plyの切り替え確認

--- Assets/Scripts/GameManager_Script.cs
+++ Assets/Scripts/GameManager_Script.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager_Script : MonoBehaviour
 {
+    [SerializeField] GameObject buy_p;
+    [SerializeField] GameObject ply_p;
+    enum state { 
+        Buying,
+        Shooting,
+        Changing
+    }
+    state NowState;
     // Start is called before the first frame update
     void Start()
     {
-        
+        NowState = state.Buying;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (NowState)
+        {
+            case state.Buying:  buying();   break;
+            case state.Shooting:Shooting(); break;
+            case state.Changing:Changing(); break;
+        }
+
+    }
+    void buying()
+    {
+
+    }
+    void Shooting()
+    {
+
+    }
+    void Changing()
+    {
+
+    }
+
+    public void ChangePanel()
+    {
+        if (NowState==state.Buying)
+        {
+            buy_p.SetActive(false);
+            ply_p.SetActive(true);
+        }
+        else
+        {
+            buy_p.SetActive(true);
+            ply_p.SetActive(false);
+        }
     }
 }
