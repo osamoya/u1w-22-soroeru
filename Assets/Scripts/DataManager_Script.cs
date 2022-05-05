e58911ba83d0c9a73a9cc1757f297cd605a6802a
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2022-05-05 17:43:17 +0900

    [Add]買い物に使うスクリプト

---
+++ Assets/Scripts/DataManager_Script.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager_Script : MonoBehaviour
+{
+    [SerializeField] int b1;//’l’i
+    [SerializeField] int b2;
+    [SerializeField] int coin;
+    
+    public void OnClickBuy()
+    {
+
+    }
+}
