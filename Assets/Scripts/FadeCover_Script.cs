e38329fd16deca52c6d7042750a1810550293757
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2022-05-05 17:26:32 +0900

    [Add]Fadeさせるスクリプト。エディタではエラってるが問題ない

---
+++ Assets/Scripts/FadeCover_Script.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FadeCover_Script : MonoBehaviour
+{
+    Image p;
+    [SerializeField] GameObject panel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        p = panel.GetComponent<Image>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClickFade()
+    {
+        Debug.Log("fade");
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(p.DOFade(1, 1f));
+        sequence.Append(p.DOFade(0, 1f));
+        //DOFade(0, 1f).;
+        sequence.Play();
+        
+    }
+}
