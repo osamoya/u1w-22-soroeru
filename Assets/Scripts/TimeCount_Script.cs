cbb9f82c66699f97f7af97aa26b32ff48c41704c
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2022-05-07 08:58:18 +0900

    [Add]時間の概念を追加

---
+++ Assets/Scripts/TimeCount_Script.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCount_Script : MonoBehaviour
+{
+    float timeCount;
+    bool isCounting;
+    int deg = 0;
+    int dispM=0;
+    int dispH=12;
+    string s;
+    Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isCounting = true;
+        text = GetComponent<Text>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCounting)
+        {
+            timeCount += Time.deltaTime;
+            //timecountÇ™1Çí¥Ç¶ÇΩèÍçá
+            if (timeCount >= 1)
+            {
+                dispM++;
+                timeCount -= 1;//MÇÇPÇ…ÇµÇƒÅA1à¯Ç≠
+
+            }
+
+            //MÇ™60Ç…Ç»Ç¡ÇΩèuä‘Ç…1ë´Ç∑
+            if (dispM >= 60)
+            {
+                dispH++;
+                dispM = 0;
+            }
+            if (dispH == 24)//íËéû
+            {
+                //Ç±Ç±Ç…âΩÇÁÇ©ÇÃèàóù
+                isCounting = false;
+            }
+            s = "" +dispH+":"+dispM;
+        }
+        else
+        {
+            s = "CLOSE";
+        }
+        //ï∂éöóÒÇ…Ç∑ÇÈ
+        text.text = s;
+    }
+}
