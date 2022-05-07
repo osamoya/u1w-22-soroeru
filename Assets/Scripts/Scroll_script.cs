d0b21e8e2e93137ef867e6252a17ffacf017540d
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2022-05-07 14:11:20 +0900

    [Add]背景スクロール完成

---
+++ Assets/Scripts/Scroll_script.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll_script : MonoBehaviour
+{
+    Vector3 speed = new Vector3(0, 0.1f, 0);
+    Vector3 x = new Vector3(0, -10.75f, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += speed;
+        if(transform.position.y>= 10.75)
+        {
+            transform.localPosition=x;
+        }
+    }
+}
