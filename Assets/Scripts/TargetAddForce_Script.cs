9db9a97e7ba35165e44dbe37eea823606251faf3
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2022-05-08 15:12:45 +0900

    [update]基本的に当たらないAI

--- Assets/Scripts/TargetAddForce_Script.cs
+++ Assets/Scripts/TargetAddForce_Script.cs
@@ -6,6 +6,8 @@ public class TargetAddForce_Script : MonoBehaviour
 {
     Rigidbody2D rb;
     [SerializeField] int power;
+    [SerializeField] float rad;
+    [SerializeField] float rate;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +22,20 @@ public class TargetAddForce_Script : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col)
     {
          Debug.Log("‚Í‚¢‚Á‚½");
-        float diffx = transform.position.x - col.transform.position.x;
-        rb.AddForce(new Vector2(diffx,0)*power);
+        float difx = transform.position.x - col.transform.position.x;
+        if (difx > 0)
+        {
+            Debug.Log("a:dif=" + difx);
+            difx = rate*difx - rad;
+            Debug.Log("force=" + difx);
+        }
+        else
+        {
+            Debug.Log("b:dif=" + difx);
+            difx = rate*difx + rad;
+            Debug.Log("force=" + difx);
+        }
+        
+        rb.AddForce(new Vector2(-difx,0)*power);
     }
 }
